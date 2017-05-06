b11fb1852f9aa281b56419083c30a26a23feeaad
Kingsley <kingsley.xym@gmail.com>
2017-05-06 23:37:28 +0800

    upload registerformFill.js
    a registerform-fill file which is convenient for debug.

---
+++ registerformFill.js
@@ -0,0 +1,48 @@
+registerform.teamName.value = "我们唱着东方红";
+registerform.password.value = "666666";
+registerform.passwordConfirm.value = "666666";
+registerform.competitionType.value = 1;
+
+registerform.teamLeaderName.value = "当家做主站起来";
+registerform.studentNo.value = "201122335566";
+registerform.contact.value = "13723332333";
+registerform.college.value = "生命学院";
+registerform.major.value = "膜法与长寿技术";
+registerform.grade.value = "2016级";
+registerform.campus.value = "大学城校区";
+
+registerform.teamMemberName1.value = "我们讲着春天的故事";
+registerform.studentNo1.value = "201122335566";
+registerform.contact1.value = "13723332333";
+registerform.college1.value = "生命学院";
+registerform.major1.value = "膜法与长寿技术";
+registerform.grade1.value = "2016级";
+registerform.campus1.value = "五山校区";
+
+registerform.teamMemberName2.value = "改革开放富起来";
+registerform.studentNo2.value = "201122335566";
+registerform.contact2.value = "13723332333";
+registerform.college2.value = "生命学院";
+registerform.major2.value = "膜法与长寿技术";
+registerform.grade2.value = "2016级";
+registerform.campus2.value = "五山校区";
+
+registerform.teamMemberName3.value = "继往开来的领路人";
+registerform.studentNo3.value = "201122335566";
+registerform.contact3.value = "13723332333";
+registerform.college3.value = "生命学院";
+registerform.major3.value = "膜法与长寿技术";
+registerform.grade3.value = "2016级";
+registerform.campus3.value = "五山校区";
+
+registerform.teamMemberName4.value = "带领我们走进那新时代";
+registerform.studentNo4.value = "201122335566";
+registerform.contact4.value = "13723332333";
+registerform.college4.value = "生命学院";
+registerform.major4.value = "膜法与长寿技术";
+registerform.grade4.value = "2016级";
+registerform.campus4.value = "五山校区";
+
+
+//高举旗帜 开创未来
+//同志们好~ 同志们辛苦了~
