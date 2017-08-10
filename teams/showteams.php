9d2a27bb160408c6c8a82579d9dc4b9551bdeee1
Kingsley <kingsley.xym@gmail.com>
2017-08-10 17:45:29 +0800

    Optimize SQL Query and PHP Process Code in `showteams.php`

--- teams/showteams.php
+++ teams/showteams.php
@@ -7,62 +7,76 @@ $response[0] = array( 'loggedIn' => 1);
 $index = 1;
 
 if (isset($_SESSION['teamID'])) {
-    $response[0] = array( 'loggedIn' => 0);
-    
-    foreach($connect->query('SELECT * from students where teamID = ' . $_SESSION['teamID']) as $student) {
-        $response[$index] = array(
-        	'studentName' => $student['studentName'],
-        	'studentNo' => $student['studentNo'],
-        	'contact' => $student['contact'],
-        	'campus' => $student['campus'],
-        	'college' => $student['college'],
-        	'major' => $student['major'],
-        	'grade' => $student['grade']);
-        $index++;
-    }
+	$response[0] = array( 'loggedIn' => 0);
+	foreach($connect->query('SELECT * from students where teamID = ' . $_SESSION['teamID']) as $student) {
+		$response[$index] = array(
+			'studentName' => $student['studentName'],
+			'studentNo' => $student['studentNo'],
+			'contact' => $student['contact'],
+			'campus' => $student['campus'],
+			'college' => $student['college'],
+			'major' => $student['major'],
+			'grade' => $student['grade']);
+		$index++;
+	}
 }
 
-foreach($connect->query('SELECT * from productionTeams ORDER BY teamID DESC') as $team) {
-    $stuIndex = 0;
-    foreach($connect->query('SELECT * from students where teamID = ' . $team['teamID']) as $student) {
-      $students[$stuIndex] = array(
-      	'studentName' => $student['studentName'],
-      	'campus' => $student['campus'],
-      	'college' => $student['college'],
-      	'major' => $student['major'],
-      	'grade' => $student['grade']);
-      $stuIndex++;
-    }
+foreach($connect->query(
+	'SELECT
+	productionTeams.teamID AS teamID,
+	productionTeams.teamName,
+	productionteams.registerTime,
+	students.studentID AS studentID,
+	students.studentName,
+	students.campus,
+	students.college,
+	students.major,
+	students.grade
+	FROM productionTeams
+	INNER JOIN students
+	ON productionTeams.teamID = students.teamID
+	UNION ALL
+	SELECT
+	creativityTeams.teamID AS teamID,
+	creativityTeams.teamName,
+	creativityteams.registerTime,
+	students.studentID AS studentID,
+	students.studentName,
+	students.campus,
+	students.college,
+	students.major,
+	students.grade
+	FROM creativityTeams
+	INNER JOIN students
+	ON creativityTeams.teamID = students.teamID
+	ORDER BY teamID DESC, studentID ASC
+	') as $data) {
 
-    $response[$index] = array(
-    	'teamName' => $team['teamName'],
-    	'teamID' => $team['teamID'],
-    	'calendarTime' => date("Y-m-d",strtotime($team['registerTime'])),
-    	'clockTime' => date("H:i",strtotime($team['registerTime'])),
-    	'students' => $students);
-    unset($students);
-    $index++;
+	if ((!isset($teamID)) OR ($teamID != $data['teamID'])) {
+		$teamID = $data['teamID'];
+		if (isset($students) AND sizeof($students)) {
+			$response[$index]['students'] = $students;
+			$index++;
+		};
+		$students = array(array(
+				'studentName' => $data['studentName'],
+				'campus' => $data['campus'],
+				'college' => $data['college'],
+				'major' => $data['major'],
+				'grade' => $data['grade']));
+		$response[$index] = array(
+			'teamName' => $data['teamName'],
+			'teamID' => $data['teamID'],
+			'calendarTime' => date("Y-m-d",strtotime($data['registerTime'])),
+			'clockTime' => date("H:i",strtotime($data['registerTime'])));
+	} else {
+		array_push($students, array(
+			'studentName' => $data['studentName'],
+			'campus' => $data['campus'],
+			'college' => $data['college'],
+			'major' => $data['major'],
+			'grade' => $data['grade']));
+	}
 }
-
-foreach($connect->query('SELECT * from creativityteams ORDER BY teamID DESC') as $team) {
-    $stuIndex = 0;
-    foreach($connect->query('SELECT * from students where teamID = ' . $team['teamID']) as $student) {
-      $students[$stuIndex] = array(
-      	'studentName' => $student['studentName'],
-      	'campus' => $student['campus'],
-      	'college' => $student['college'],
-      	'major' => $student['major'],
-      	'grade' => $student['grade']);
-      $stuIndex++;
-    }
-    $response[$index] = array(
-    	'teamName' => $team['teamName'],
-    	'teamID' => $team['teamID'],
-    	'calendarTime' => date("Y-m-d",strtotime($team['registerTime'])),
-    	'clockTime' => date("H:i",strtotime($team['registerTime'])),
-    	'students' => $students);
-    unset($students);
-    $index++;
-}
-
+$response[$index]['students'] = $students;
 echo json_encode($response);
