db3af7a577aa789ed8bc0fce4661ec0e16e662aa
Kingsley <kingsley.xym@gmail.com>
2017-08-11 17:05:30 +0800

    :sparkles: Reset Function Done

--- login/reset.php
+++ login/reset.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 header('Content-Type: application/json');
 require_once('../assets/config.php');
 
@@ -7,10 +8,23 @@ blankCheck('teamID', 'studentNo', 'contact', 'newPassword', 'newPasswordConfirm'
 if (strtolower($_POST['captcha']) != $_SESSION['captcha']) response(2, '验证码错误！');
 if ($_POST['newPassword'] !== $_POST['newPasswordConfirm']) response(3, '两次输入密码不一致！');
 
-$sql = 'SELECT * from `' . ($_POST['teamID'] > 2000 ? 'creativityteams' : 'productionteams') . '` WHERE teamID = ? AND pwdSHA256 = ?';
+$sql = '
+SELECT * FROM `students`
+WHERE `teamID` = ? AND
+`studentNo` = ? AND
+`contact` = ? AND
+`teamCharacter` = \'teamLeader\'';
 $stmt = $connect->prepare($sql);
-$stmt->execute(array($_POST['teamID'], hash('sha256', $_POST['newPassword'])));
+$stmt->execute(array($_POST['teamID'], $_POST['studentNo'], $_POST['contact']));
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-if (empty($result)) response(4, '队伍 ID 或登录密码错误！');
+if (empty($result)) response(4, '验证失败，请确认相关信息填写无误');
+
+$sql = 'UPDATE `' . ($_POST['teamID'] > 2000 ? 'creativityteams' : 'productionteams') . '`
+SET `pwdSHA256` = ?
+WHERE teamID = ?';
+$stmt = $connect->prepare($sql);
+$stmt->execute(array(hash('sha256', $_POST['newPassword']), $_POST['teamID']));
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (!empty($result)) response(5, '密码更新失败，请重新尝试');
 
 response(0);
