7006b63ca0b9b1c5f96194d843d2ff13155192ba
Kingsley <kingsley.xym@gmail.com>
2017-08-11 20:30:22 +0800

    Update register Back-End Code

--- register/register.php
+++ register/register.php
@@ -5,29 +5,49 @@ require_once('../assets/config.php');
 
 $_POST['teamName'] = str_replace(array(" ", "\t", "\r"), "", $_POST['teamName']);
 
-existCheck('teamName', 'password', 'passwordConfirm', 'competitionType', 'captcha', 'teamLeaderName', 'studentNo', 'contact', 'college', 'major', 'grade', 'campus');
-blankCheck('teamName', 'password', 'passwordConfirm', 'competitionType', 'captcha', 'teamLeaderName', 'studentNo', 'contact', 'college', 'major', 'grade', 'campus');
+existCheck('teamName', 'password', 'passwordConfirm', 'competitionType',
+	'captcha', 'teamLeaderName', 'studentNo', 'contact',
+	'college', 'major', 'grade', 'campus');
+blankCheck('teamName', 'password', 'passwordConfirm', 'competitionType',
+	'captcha', 'teamLeaderName', 'studentNo', 'contact',
+	'college', 'major', 'grade', 'campus');
+
 if (strtolower($_POST['captcha']) != $_SESSION['captcha']) response(2, '验证码错误！');
 if ($_POST['password'] !== $_POST['passwordConfirm']) response(3, '两次输入密码不一致！');
 
 $competitionType = $_POST['competitionType'] == 2 ? 'creativityteams' : 'productionteams';
 
-$sql = 'SELECT * from `' . $competitionType . '` WHERE teamName = ?';
+$sql = '
+SELECT *
+from `' . $competitionType . '`
+WHERE teamName = ?';
 $stmt = $connect->prepare($sql);
 $stmt->execute(array($_POST['teamName']));
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if (!empty($result)) response(4, '该队名已存在，请更换');
 
-$query = $connect->query('SELECT `AUTO_INCREMENT` FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = \'' . $competitionType . '\'')->fetch(PDO::FETCH_ASSOC);
+$query = $connect->query('
+	SELECT `AUTO_INCREMENT`
+	FROM INFORMATION_SCHEMA.TABLES
+	WHERE TABLE_NAME = \'' . $competitionType . '\'')->fetch(PDO::FETCH_ASSOC);
 $nextID = $query['AUTO_INCREMENT'];
+$salt = sha1((mt_rand()));
 
-$sql = 'INSERT INTO `'. $competitionType .'` (`teamName`, `registerTime`, `pwdSHA256`) VALUES (?,NOW(),?)';
+$sql = '
+INSERT INTO `'. $competitionType .'`
+(`teamName`, `registerTime`, `salt`, `saltedPasswordHash`)
+VALUES
+(?, NOW(), ?, ?)';
 $stmt = $connect->prepare($sql);
-$stmt->execute(array($_POST['teamName'], hash('sha256', $_POST['password'])));
+$stmt->execute(array($_POST['teamName'], $salt, hash('sha256', $_POST['password'] . $salt)));
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if (!empty($result)) response(5, '报名失败，请重试');
 
-$sql = 'INSERT INTO `students` (`studentName`, `studentNo`, `contact`, `campus`, `college`, `major`, `grade`, `teamID`, `teamCharacter`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+$sql = '
+INSERT INTO `students`
+(`studentName`, `studentNo`, `contact`, `campus`, `college`, `major`, `grade`, `teamID`, `teamCharacter`)
+VALUES
+(?, ?, ?, ?, ?, ?, ?, ?, ?)';
 $stmt = $connect->prepare($sql);
 $stmt->execute(array(
 	$_POST['teamLeaderName'],
@@ -47,7 +67,11 @@ for ($i = 1; $i < 5; $i++) {
 		($_POST['major' . $i] !== '') AND 
 		($_POST['grade' . $i] !== 0) AND 
 		($_POST['campus' . $i] !== 0)) {
-		$sql = 'INSERT INTO `students` (`studentName`, `studentNo`, `contact`, `campus`, `college`, `major`, `grade`, `teamID`, `teamCharacter`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+		$sql = '
+	INSERT INTO `students`
+	(`studentName`, `studentNo`, `contact`, `campus`, `college`, `major`, `grade`, `teamID`, `teamCharacter`)
+	VALUES
+	(?, ?, ?, ?, ?, ?, ?, ?, ?)';
 		$stmt = $connect->prepare($sql);
 		$stmt->execute(array(
 			$_POST['teamMemberName' . $i],
