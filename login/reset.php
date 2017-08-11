ea821cd236df496c24592d728524d5238b84c32d
Kingsley <kingsley.xym@gmail.com>
2017-08-11 21:10:00 +0800

    Optimize reset with `newSalt` Value

--- login/reset.php
+++ login/reset.php
@@ -21,17 +21,14 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if (empty($result)) response(4, '验证失败，请确认相关信息填写无误');
 
 $competitionType = $_POST['teamID'] > 2000 ? 'creativityteams' : 'productionteams';
-$query = $connect->query('
-	SELECT salt
-	FROM `' . $competitionType . '`
-	WHERE teamID = '. $_POST['teamID'])->fetch(PDO::FETCH_ASSOC);
 
+$newSalt = sha1((mt_rand()));
 $sql = '
 UPDATE `' . $competitionType . '`
-SET `saltedPasswordHash` = ?
+SET `salt` = ?, `saltedPasswordHash` = ?
 WHERE teamID = ' . $_POST['teamID'];
 $stmt = $connect->prepare($sql);
-$stmt->execute(array(hash('sha256', $_POST['newPassword'] . $query['salt'])));
+$stmt->execute(array($newSalt, hash('sha256', $_POST['newPassword'] . $newSalt)));
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if (!empty($result)) response(5, '密码更新失败，请重新尝试');
 
